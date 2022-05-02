f8fd7c092acefe03fac2a6791e6d6eeca0d248b9
Ekaterina Strakhova <mikopashpe@gmail.com>
2022-05-02 17:55:00 +0500

    2 may

---
+++ Ex29.py
@@ -0,0 +1,8 @@
+'''
+Напишите программу, которая выводит слова «Python is awesome!» (без кавычек) 10 раз.
+'''
+
+for i in range(10):
+    print('Python is awesome!')
+
+    
---
+++ Ex31.py
@@ -0,0 +1,16 @@
+'''
+Напишите программу, которая использует ровно три цикла for для печати следующей последовательности символов:
+'''
+
+a = 'AAA'
+b = 'BBBB'
+t = 'TTTTT'
+for i in range(0, 6):
+    print(a)
+for i in range(0, 5):
+    print(b)
+print('E')
+for i in range(0, 9):
+    print(t)
+print('G')
+
