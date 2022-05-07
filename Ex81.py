4a7d678b86148a738c6373f6a718333d4728a064
Ekaterina Strakhova <mikopashpe@gmail.com>
2022-05-07 17:09:07 +0500

    67 may

---
+++ Ex81.py
@@ -0,0 +1,15 @@
+'''
+
+'''
+
+
+numbers = [8, 9, 10, 11]
+numbers[1] = 17
+numbers.append(4)
+numbers.append(5)
+numbers.append(6)
+del numbers[0]
+numbers = numbers+numbers
+numbers.insert(3, 25)
+print(numbers)
+
