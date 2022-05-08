d88e5bc1f5924df6849887e91e3d1ec22716e82a
Ekaterina Strakhova <mikopashpe@gmail.com>
2022-05-08 16:14:19 +0500

    8 may

---
+++ Ex88.py
@@ -0,0 +1,14 @@
+'''
+Напишите функцию print_digit_sum(), которая принимает одно целое число num и выводит на печать сумму его цифр.
+'''
+
+def print_digit_sum(num):
+    l =[int(i) for i in str(n)]
+    print(sum(l))
+
+# считываем данные
+n = int(input())
+
+# вызываем функцию
+print_digit_sum(n)
+
